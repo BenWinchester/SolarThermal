f1a46c8f6dda39a10cb9ef07a915246a602bffb8
Ben Winchester <benedict.winchester@gmail.com>
2022-05-06 14:30:13 +0100

    :white_check_mark: Add Github and python files

---
+++ src/solar_thermal/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+########################################################################################
+# __init__.py - Python internals module, no functionality here.                        #
+#                                                                                      #
+# Author: Ben Winchester                                                               #
+# Copyright: Ben Winchester, 2022                                                      #
+# Date created: 06/05/2022                                                             #
+# License: Open source                                                                 #
+########################################################################################
