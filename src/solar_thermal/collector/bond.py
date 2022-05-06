66b3eed7f73bcc3351d3fcb6774910d462b9631a
Ben Winchester <benedict.winchester@gmail.com>
2022-05-06 17:00:57 +0100

    Create bond layer module.

---
+++ src/solar_thermal/collector/bond.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3.7
+########################################################################################
+# collector/bond.py - Represents bond layer within a collector.                        #
+#                                                                                      #
+# Author: Ben Winchester                                                               #
+# Copyright: Ben Winchester, 2022                                                      #
+# Date created: 06/05/2022                                                             #
+# License: Open source                                                                 #
+########################################################################################
+
+"""
+The bond module for the collector component model.
+
+This module represents a bond layer within a solar-thermal collector.
+
+"""
+
+from dataclasses import dataclass
+
+from .__utils__ import MicroLayer
+
+__all__ = ("Bond",)
+
+
+@dataclass
+class Bond(MicroLayer):
+    """
+    Represents an Bond layer within the panel.
+
+    .. attribute:: width
+        The width of the bond, measured in meters.
+
+    """
+
+    width: float
