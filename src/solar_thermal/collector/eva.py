1f04d22fd4286bf312c241e300ae8b818f6c897d
Ben Winchester <benedict.winchester@gmail.com>
2022-05-06 17:05:29 +0100

    Add EVA module

---
+++ src/solar_thermal/collector/eva.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3.7
+########################################################################################
+# collector/eva.py - Represents eva layer within a collector.                          #
+#                                                                                      #
+# Author: Ben Winchester                                                               #
+# Copyright: Ben Winchester, 2022                                                      #
+# Date created: 06/05/2022                                                             #
+# License: Open source                                                                 #
+########################################################################################
+
+"""
+The eva module for the collector component.
+
+This module represents a eva layer within a PV-T panel.
+
+"""
+
+from .__utils__ import MicroLayer
+
+__all__ = ("EVA",)
+
+
+class EVA(MicroLayer):
+    """
+    Represents an EVA layer within the collector.
+
+    """
